0cc4779cbb1285f106cdc33c7b86144010ed7bbd
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-14 21:32:04 -0300

    First screen has been implemented

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,58 @@ class BytebankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: [
-            Image.asset('images/bytebank_logo.png'),
-            Container(
-              color: Colors.green,
-              height: 120,
-              width: 100,
+    theme: ThemeData(
+    primaryColor: Colors.green[900],
+    colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.green),
+    buttonTheme: ButtonThemeData(
+    buttonColor: Colors.blueAccent[700],
+    textTheme: ButtonTextTheme.primary,
+    )
+    ),
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset('images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              color: Theme.of(context).primaryColor,
+              height: 100,
+              width: 150,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.people),
-                  Text('Contacts')
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                  Text('Contacts', style: TextStyle(color: Colors.white, fontSize: 20.0))
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
