fe59e01347dcad1a17665f207d11b73e0cbcc7c6
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-14 21:07:41 -0300

    Dashboard image has been adicioned

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,29 @@ import 'package:flutter/material.dart';
 void main() => runApp(BytebankApp());
 
 class BytebankApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Dashboard'),),
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+        body: Column(
+          children: [
+            Image.asset('images/bytebank_logo.png'),
+            Container(
+              color: Colors.green,
+              height: 120,
+              width: 100,
+              child: Column(
+                children: [
+                  Icon(Icons.people),
+                  Text('Contacts')
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
