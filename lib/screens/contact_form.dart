cd37c91cfb1479543de15cd3a4bd165523f0d6b7
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-28 20:58:08 -0300

    Form screen has been implemented

---
+++ lib/screens/contact_form.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/models/contact.dart';
+
+class ContactForm extends StatefulWidget {
+
+  @override
+  State<ContactForm> createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _accountNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New contact'),),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Full name'
+                ),
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+            TextField(
+              controller: _accountNumberController,
+              decoration: InputDecoration(
+                labelText: 'Account number'
+              ),
+              style: TextStyle(fontSize: 24.0),
+              keyboardType: TextInputType.number,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text('Create'), onPressed: () {
+                    String fullName = _nameController.text;
+                    final int? accountNumber = int.tryParse(_accountNumberController.text);
+
+                    if(fullName != null && accountNumber != null) {
+                       final Contact newContact = Contact(fullName, accountNumber);
+                       Navigator.pop(context, newContact);
+                    }
+                },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
