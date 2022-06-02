13b8c89353998754975c67e7f51044082867842f
Gabriel Simon <gabrielsimon775@gmail.com>
2022-06-02 20:13:32 -0300

    Add new functionalily of button Transaction feed

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/screens/contacts_list.dart';
 
@@ -18,24 +19,47 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/bytebank_logo.png'),
           ),
-          Row(
-            children: [
-              _FutureItem('Transfer', Icons.monetization_on),
-              _FutureItem('Transaction Feed', Icons.description),
-            ],
-          ),
+            Container(
+              height: 120,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  _FeatureItem(
+                    'Transfer',
+                    Icons.monetization_on,
+                    onClick: () {
+                      _showContactsList(context);
+                    },
+                  ),
+                  _FeatureItem(
+                    'Transaction Feed',
+                    Icons.description,
+                    onClick: () => print('is being implemeting'),
+                  ),
+                ],
+              ),
+            ),
+
         ],
       ),
     );
   }
 }
 
-class _FutureItem extends StatelessWidget {
+void _showContactsList(BuildContext context) {
+  Navigator.of(context).push(
+    MaterialPageRoute(
+      builder: (context) => ContactsList(),
+    ),
+  );
+}
 
+class _FeatureItem extends StatelessWidget {
   final String name;
   final IconData icon;
+  final Function onClick;
 
-  _FutureItem(this.name, this.icon);
+  _FeatureItem(this.name, this.icon, {required this.onClick});
 
   @override
   Widget build(BuildContext context) {
@@ -44,9 +68,7 @@ class _FutureItem extends StatelessWidget {
       child: Material(
         color: Theme.of(context).primaryColor,
         child: InkWell(
-          onTap: (){
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => ContactsList()));
-          },
+          onTap: () => onClick(),
           child: Container(
             padding: EdgeInsets.all(8.0),
             height: 100,
@@ -60,7 +82,8 @@ class _FutureItem extends StatelessWidget {
                   color: Colors.white,
                   size: 24,
                 ),
-                Text(name, style: TextStyle(color: Colors.white, fontSize: 20.0))
+                Text(name,
+                    style: TextStyle(color: Colors.white, fontSize: 20.0))
               ],
             ),
           ),
